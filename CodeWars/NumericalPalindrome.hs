2a7f184729ad59d53135aaed1cc826c329cb4a04
philg112 <pgrujovski@protonmail.com>
2023-10-11 16:03:42 +1100

    not so easey

---
+++ CodeWars/NumericalPalindrome.hs
@@ -0,0 +1,10 @@
+module CodeWars.NumericalPalindrome where
+
+-- https://www.codewars.com/kata/58df8b4d010a9456140000c7/train/haskell
+
+palindrome :: Int -> Maybe Int
+palindrome n = _
+
+isPalindrome :: String -> Bool
+isPalindrome [a] = False
+isPalindrome xs = reverse xs == xs
